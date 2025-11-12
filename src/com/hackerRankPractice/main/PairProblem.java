2b4d15812dcd51a48645eb8d2ed5dc420eceebf2
ckonduru <Chandra.Konduru@caremore.com>
2025-11-11 17:30:52 -0800

    Updated the pair problem code

--- src/com/hackerRankPractice/main/PairProblem.java
+++ src/com/hackerRankPractice/main/PairProblem.java
@@ -1,5 +1,13 @@
 package com.hackerRankPractice.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*Problem Statement:
+ * Given an array of integers nums, return the number of good pairs. A pair (i,
+ * j) is called good if nums[i] == nums[j] and i < j. 1 <= nums.length <= 100 1
+ * <= nums[i] <= 100
+ */
 public class PairProblem {
 	
 public int numIdenticalPairs(int[] nums) {
@@ -15,4 +23,23 @@ public int numIdenticalPairs(int[] nums) {
 	}
 	return count;
     }
+
+public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter array elements separated by spaces:");
+    String[] numStrs = reader.readLine().trim().split(" ");
+    int[] nums = new int[numStrs.length];
+    for (int i = 0; i < numStrs.length; i++) {
+        nums[i] = Integer.parseInt(numStrs[i]);
+    }
+   
+
+    PairProblem pair = new PairProblem();
+    int result = pair.numIdenticalPairs(nums);
+    System.out.print("Result: "+ result);
+    
+    System.out.println();
+}
+	
+
 }
